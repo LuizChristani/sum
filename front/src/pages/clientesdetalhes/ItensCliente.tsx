ac208ac0f4a712c3992ba072a030cb4e680dddac
LuizChristani <166182040+LuizChristani@users.noreply.github.com>
2024-11-19 08:21:41 -0300

    Delete front directory

--- front/src/pages/clientesdetalhes/ItensCliente.tsx
+++
@@ -1,9 +0,0 @@
-const ItensCliente = () => {
-    return (
-        <div className="flex justify-between p-4 w-full h-[10vh] bg-black items-center">
-            <span className="text-gray-400 font-semibold pl-[9vh] text-3xl">Informações do cliente</span>
-        </div>
-    );
-};
-
-export default ItensCliente;
